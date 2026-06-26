1364c727fca98a445ee3cf9cefd1dd596a9a5b9b
root <root@localhost.localdomain>
2026-06-26 12:43:23 +0000

    Initial Commit - High-End Kotlin Template

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
   alias(libs.plugins.android.application)
   alias(libs.plugins.kotlin.compose)
   alias(libs.plugins.google.devtools.ksp)
-
+  alias(libs.plugins.kotlin.android)
   alias(libs.plugins.google.services)
 }
 
@@ -20,6 +20,9 @@ android {
     versionName = "1.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+  }
+  kotlinOptions {
+    jvmTarget = "11"
   }
 
   signingConfigs {
